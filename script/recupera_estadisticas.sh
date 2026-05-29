fbd1e32c35d7428a8818350a3cca10dff6461456
Marzerp <araceliromerozerpa@gmail.com>
2026-05-29 13:02:39 -0600

    script

--- script/recupera_estadisticas.sh
+++ script/recupera_estadisticas.sh
@@ -12,60 +12,40 @@ do
 
     suma=0
     suma2=0
-    validos=0
 
     for RUN in $(seq 1 10)
     do
         echo "Run $RUN"
 
-        # Capturar SOLO el tiempo
+        # Ejecutar y capturar tiempo real
         T=$(
             (/usr/bin/time -f "%e" \
             mpiexec -n $N -f ../machinefile \
             ./hough_mpi ./images/bordes_binarios.jpg \
             > /dev/null) 2>&1
-
         )
 
-        # Validar que sea numérico
-        if [[ $T =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "$N,$RUN,$T" >> $OUTPUT
 
-            echo "$N,$RUN,$T" >> $OUTPUT
-
-            suma=$(awk "BEGIN {print $suma + $T}")
-            suma2=$(awk "BEGIN {print $suma2 + ($T * $T)}")
-
-            validos=$((validos + 1))
-
-        else
-            echo "Run $RUN falló"
-        fi
+        # Acumular para media y desviación
+        suma=$(awk "BEGIN {print $suma + $T}")
+        suma2=$(awk "BEGIN {print $suma2 + ($T * $T)}")
 
     done
 
-    if [ $validos -gt 0 ]; then
+    media=$(awk "BEGIN {print $suma / 10}")
 
-        media=$(awk "BEGIN {print $suma / $validos}")
+    desviacion=$(awk "BEGIN {
+        mean = $media;
+        variance = ($suma2 / 10) - (mean * mean);
+        print sqrt(variance);
+    }")
 
-        desviacion=$(awk "BEGIN {
-            mean = $media;
-            variance = ($suma2 / $validos) - (mean * mean);
-
-            if (variance < 0)
-                variance = 0;
-
-            print sqrt(variance);
-        }")
-
-        echo ""
-        echo "Procesos: $N"
-        echo "Media: $media s"
-        echo "Desviacion estandar: $desviacion s"
-        echo ""
-
-    else
-        echo "Todos los runs fallaron"
-    fi
+    echo ""
+    echo "Procesos: $N"
+    echo "Media: $media s"
+    echo "Desviacion estandar: $desviacion s"
+    echo ""
 
 done
 
