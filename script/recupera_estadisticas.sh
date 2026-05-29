f8e9c681bcae6a8a738911e69252564ed61472c4
Marzerp <araceliromerozerpa@gmail.com>
2026-05-29 12:11:52 -0600

    changes

--- script/recupera_estadisticas.sh
+++ script/recupera_estadisticas.sh
@@ -4,7 +4,7 @@ OUTPUT="resultados.csv"
 
 echo "procesos,ejecucion,tiempo" > $OUTPUT
 
-for N in 1 2 3 4
+for N in 1 2 3 4 5 6 7 8
 do
     echo "======================================"
     echo "Benchmark con $N procesos"
@@ -12,41 +12,61 @@ do
 
     suma=0
     suma2=0
+    validos=0
 
     for RUN in $(seq 1 10)
     do
         echo "Run $RUN"
 
-        # Ejecutar y capturar tiempo real
+        # Capturar SOLO el tiempo
         T=$(
             (/usr/bin/time -f "%e" \
             mpiexec -n $N -f ../machinefile \
-            ./hough_mpi bordes_binarios.jpg \
-            > /dev/null) 2>&1
+            ./hough_mpi ./images/bordes_binarios.jpg \
+            > /dev/null 2>/dev/null) 2>&1
         )
 
-        echo "$N,$RUN,$T" >> $OUTPUT
+        # Validar que sea numérico
+        if [[ $T =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
 
-        # Acumular para media y desviación
-        suma=$(awk "BEGIN {print $suma + $T}")
-        suma2=$(awk "BEGIN {print $suma2 + ($T * $T)}")
+            echo "$N,$RUN,$T" >> $OUTPUT
+
+            suma=$(awk "BEGIN {print $suma + $T}")
+            suma2=$(awk "BEGIN {print $suma2 + ($T * $T)}")
+
+            validos=$((validos + 1))
+
+        else
+            echo "Run $RUN falló"
+        fi
 
     done
 
-    media=$(awk "BEGIN {print $suma / 10}")
+    if [ $validos -gt 0 ]; then
 
-    desviacion=$(awk "BEGIN {
-        mean = $media;
-        variance = ($suma2 / 10) - (mean * mean);
-        print sqrt(variance);
-    }")
+        media=$(awk "BEGIN {print $suma / $validos}")
 
-    echo ""
-    echo "Procesos: $N"
-    echo "Media: $media s"
-    echo "Desviacion estandar: $desviacion s"
-    echo ""
+        desviacion=$(awk "BEGIN {
+            mean = $media;
+            variance = ($suma2 / $validos) - (mean * mean);
+
+            if (variance < 0)
+                variance = 0;
+
+            print sqrt(variance);
+        }")
+
+        echo ""
+        echo "Procesos: $N"
+        echo "Media: $media s"
+        echo "Desviacion estandar: $desviacion s"
+        echo ""
+
+    else
+        echo "Todos los runs fallaron"
+    fi
 
 done
 
 echo "Resultados guardados en $OUTPUT"
+
